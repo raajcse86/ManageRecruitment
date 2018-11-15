d619aead7b8619bb22f097566330f1773fd0a6c7
Rajasekar <rajasekar.murugesan@in.ey.com>
2018-11-15 16:28:45 +0530

    modified controller and pom files skipped test

--- EmployeeDetailsService/src/main/java/com/app/controllers/EmployeeDetailsController.java
+++ EmployeeDetailsService/src/main/java/com/app/controllers/EmployeeDetailsController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.app.services.EmployeeDetailsService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("*")
 public class EmployeeDetailsController {
 	
 	@Autowired
