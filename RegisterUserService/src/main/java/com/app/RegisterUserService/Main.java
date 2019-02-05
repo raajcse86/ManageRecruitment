b661aebed03d4aec904add1fee59aa06cee03d7b
Rajasekar <rajasekar.murugesan@in.ey.com>
2019-02-05 19:57:37 +0530

    added files for register service.

---
+++ RegisterUserService/src/main/java/com/app/RegisterUserService/Main.java
@@ -0,0 +1,24 @@
+package com.app.RegisterUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@ComponentScan(value={"com.app.controllers","com.app.repositories","com.app.services"})
+@EnableMongoRepositories(value= {"com.app.repositories"})
+public class Main extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Main.class);
+	}
+}
+
