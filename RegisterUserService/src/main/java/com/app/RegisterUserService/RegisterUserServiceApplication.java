f202ee4b87a133192859d58025f55502c524b391
raajcse86 <35726662+raajcse86@users.noreply.github.com>
2019-02-05 19:11:21 +0530

    Add files via upload
    Added a new project for Register service

---
+++ RegisterUserService/src/main/java/com/app/RegisterUserService/RegisterUserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.app.RegisterUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterUserServiceApplication.class, args);
+	}
+
+}
+
