f29cfdabb390081aab943d88a6f715c993509621
Soni Kumari <Soni2.Kumari@in.ey.com>
2019-02-05 14:52:49 +0530

    Soni: Changes for register API

---
+++ EmployeeDetailsService/src/main/java/com/app/models/RegisterDetails.java
@@ -0,0 +1,69 @@
+package com.app.models;
+
+import javax.validation.constraints.NotBlank;
+
+public class RegisterDetails {
+
+	private String firstName;
+
+	private String lastName;
+
+	@NotBlank
+	private String username;
+	
+	@NotBlank
+	private String password;
+	
+	private String emailId;
+	
+	private String status;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
---
+++ EmployeeDetailsService/src/main/java/com/app/services/RegisterService.java
@@ -0,0 +1,9 @@
+package com.app.services;
+
+import com.app.models.RegisterDetails;
+
+public interface RegisterService {
+
+	public void save(RegisterDetails registerDetails);
+
+}
