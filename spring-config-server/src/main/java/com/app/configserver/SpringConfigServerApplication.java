ebb80c3a2d0ab4480fab6a82c77da041a760432b
Rajasekar <rajasekar.murugesan@in.ey.com>
2019-02-12 00:30:30 +0530

    modified the code for cloud config as client in service discovery

--- spring-config-server/src/main/java/com/app/configserver/SpringConfigServerApplication.java
+++ spring-config-server/src/main/java/com/app/configserver/SpringConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.app.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringConfigServerApplication {
 
 	public static void main(String[] args) {
